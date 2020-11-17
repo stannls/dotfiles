38fccceae2e1e7e8109e728256dec8a1ce058e90
Yannis Storrer <yannis.storrer@web.de>
2020-11-17 20:59:52 +0100

    Improved update script

--- .config/polybar/scripts/check-all-updates.sh
+++ .config/polybar/scripts/check-all-updates.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
+#source https://github.com/x70b1/polybar-scripts
 
+LOCK=/tmp/checkupdatelock
+while [ -f "$LOCK" ]; do
+sleep 0.2
+done
+
+touch /tmp/checkupdatelock
 if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
     updates_arch=0
 fi
 
-if ! updates_aur=$(yay -Qum 2> /dev/null | wc -l); then
-# if ! updates_aur=$(cower -u 2> /dev/null | wc -l); then
-# if ! updates_aur=$(trizen -Su --aur --quiet | wc -l); then
-# if ! updates_aur=$(pikaur -Qua 2> /dev/null | wc -l); then
-# if ! updates_aur=$(rua upgrade --printonly 2> /dev/null | wc -l); then
+if ! updates_aur=$(trizen -Su --aur --quiet | wc -l); then
     updates_aur=0
 fi
 
 updates=$(("$updates_arch" + "$updates_aur"))
 
 if [ "$updates" -gt 0 ]; then
-    echo "$updates"
+    echo " $updates"
 else
-    echo ""
+    echo "0"
 fi
+rm /tmp/checkupdatelock
