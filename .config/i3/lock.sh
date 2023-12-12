ddcfde0a8fa7d622070953c0c18d56705f4e6cb1
Yannis Storrer <yannis.storrer@web.de>
2023-12-12 21:05:56 +0100

    Updated dotfiles (first time in round about 2 years)

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -1,4 +1,1 @@
-#!/bin/bash
-pkill -u "$USER" -USR1 dunst
-i3lockr -b 25
-pkill -u "$USER" -USR2 dunst
+i3lockr --blur 25 -- --ignore-empty-password -- -i /usr/share/wallpapers/feh/cyberpunk.jpg
--- .config/polybar/launch-bspwm.sh
+++ .config/polybar/launch-bspwm.sh
@@ -8,7 +8,6 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 for m in $(polybar --list-monitors | cut -d":" -f1); do
 	WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar -c ~/.config/polybar/config-bspwm --reload mainbar-bspwm &
-	sleep 0.2
 done
 
 echo "Bars launched..."
