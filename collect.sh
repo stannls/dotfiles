1936a7f1848663ab1837815e45f941cb591fa36c
Yannis Storrer <yannis.storrer@web.de>
2023-12-12 21:05:30 +0100

    Updated collect file

--- collect.sh
+++ collect.sh
@@ -4,5 +4,6 @@ cp ~/.config/bspwm/* .config/bspwm
 cp -r ~/.config/polybar/* .config/polybar
 cp ~/.config/sxhkd/* .config/sxhkd
 cp ~/.config/nvim/init.vim .config/nvim/init.vim
+cp ~/.config/nvim/coc-settings.json .config/nvim/coc-settings.json
 cp ~/.config/alacritty/alacritty.yml .config/alacritty/alacritty.yml
 cp ~/.config/dunst/dunstrc .config/dunst/dunstrc
